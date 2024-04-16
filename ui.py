d4a360765688ab307820ee6e3617d4aea900f38a
DESKTOP-FDBRA0R\mscho <mscho>
2024-04-16 09:57:33 +0900

    Feat. edit button click event

--- ui.py
+++ ui.py
@@ -1,64 +1,32 @@
-import os
 import sys
 
-from PyQt5.QtWidgets import *
 from PyQt5 import uic
-def resource_path(relative_path):
-    base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base_path, relative_path)
+from PyQt5.QtWidgets import *
+import resource_rc
+form_class = uic.loadUiType("./start.ui")[0]
 
-form_start = resource_path('start.ui')
-form_class = uic.loadUiType(form_start)[0]
+form_checknoisewindow = uic.loadUiType("./check_noise.ui")[0]
 
-form_check_noise = resource_path('check_noise.ui')
-form_checknoisewindow = uic.loadUiType(form_check_noise)[0]
+form_selectwordwindow = uic.loadUiType("./select_word.ui")[0]
 
-form_select_word = resource_path('select_word.ui')
-form_selectwordwindow = uic.loadUiType(form_select_word)[0]
+form_noisedwindow = uic.loadUiType("./noise.ui")[0]
 
-form_noise = resource_path('noise.ui')
-form_noisedwindow = uic.loadUiType(form_noise)[0]
+form_resultdwindow = uic.loadUiType("./result.ui")[0]
 
-form_result = resource_path('result.ui')
-form_resultdwindow = uic.loadUiType(form_result)[0]
+form_speakdwindow = uic.loadUiType("./speak.ui")[0]
 
-form_speak = resource_path('speak.uiui')
-form_speakdwindow = uic.loadUiType(form_speak)[0]
 
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-    def btn_main_to_second(self):
+    def nextClick(self):
         self.hide()
-        self.second = check_noisewindow()
+        self.second = select_wordwindow()
         self.second.exec()
         self.show()
 
-class check_noisewindow(QDialog,QWidget,form_checknoisewindow):
-    def __init__(self):
-        super(check_noisewindow,self).__init__()
-        self.initUi()
-        self.show()
-
-    def initUi(self):
-        self.setupUi(self)
-
-    def btn_second_to_main(self):
-        self.close()
-
-class selectwindow(QDialog,QWidget,form_selectwordwindow):
-    def __init__(self):
-        super(selectwindow,self).__init__()
-        self.initUi()
-        self.show()
-
-    def initUi(self):
-        self.setupUi(self)
-
-    def btn_second_to_main(self):
-        self.close()
 class select_wordwindow(QDialog,QWidget,form_selectwordwindow):
     def __init__(self):
         super(select_wordwindow,self).__init__()
@@ -68,8 +36,71 @@ class select_wordwindow(QDialog,QWidget,form_selectwordwindow):
     def initUi(self):
         self.setupUi(self)
 
-    def btn_second_to_main(self):
-        self.close()
+    def nextcheckNoise(self):
+        self.hide()
+        self.second = check_noisewindow()
+        self.second.exec()
+        self.show()
+class check_noisewindow(QDialog,QWidget,form_checknoisewindow):
+    def __init__(self):
+        super(check_noisewindow,self).__init__()
+        self.initUi()
+        self.show()
+
+    def initUi(self):
+        self.setupUi(self)
+
+    def nextNoise(self):
+        self.hide()
+        self.second = noisewindow()
+        self.second.exec()
+        self.show()
+
+class noisewindow(QDialog,QWidget,form_noisedwindow):
+    def __init__(self):
+        super(noisewindow,self).__init__()
+        self.initUi()
+        self.show()
+
+    def initUi(self):
+        self.setupUi(self)
+
+    def nextSpeak(self):
+        self.hide()
+        self.second = speakwindow()
+        self.second.exec()
+        self.show()
+
+class speakwindow(QDialog,QWidget,form_speakdwindow):
+    def __init__(self):
+        super(speakwindow,self).__init__()
+        self.initUi()
+        self.show()
+
+    def initUi(self):
+        self.setupUi(self)
+
+    def nextResult(self):
+        self.hide()
+        self.second = resultwindow()
+        self.second.exec()
+        self.show()
+
+class resultwindow(QDialog,QWidget,form_resultdwindow):
+    def __init__(self):
+        super(resultwindow,self).__init__()
+        self.initUi()
+        self.show()
+
+    def initUi(self):
+        self.setupUi(self)
+
+    def nextClick(self):
+        self.hide()
+        self.second = form_resultdwindow()
+        self.second.exec()
+        self.show()
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     myWindow = WindowClass()
