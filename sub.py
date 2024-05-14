67347bcaa50ee225f6c3d061bea2e106c597376c
mINUs <jx090425@gmail.com>
2024-05-14 21:57:52 +0900

    change similarity algorithm

--- sub.py
+++ sub.py
@@ -10,20 +10,11 @@ from PyQt5.QtCore import *
 import time
 import math
 import record
-import torchvision
-import torchvision.datasets as dset
+
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader,Dataset
-import matplotlib.pyplot as plt
-import torchvision.utils
-import numpy as np
-import random
 from PIL import Image
 import torch
-from torch.autograd import Variable
-import PIL.ImageOps
 import torch.nn as nn
-from torch import optim
 import torch.nn.functional as F
 
 import button_rc
@@ -209,28 +200,6 @@ class WindowClass(QMainWindow, form_class):
 
 
     #list = ['안녕하세요', '바가지','도깨비','고구마','누룽지','주전자']
-# ----------------------------------------------------------------
-
-
-
-# ----------------------------------------------------------------
-class Config():
-    testing_dir = "./testing"
-
-
-def imshow(img, text=None, should_save=False):
-    npimg = img.numpy()
-    plt.axis("off")
-    if text:
-        plt.text(75, 8, text, style='italic', fontweight='bold',
-                 bbox={'facecolor': 'white', 'alpha': 0.8, 'pad': 10})
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-
-def show_plot(iteration, loss):
-    plt.plot(iteration, loss)
-    plt.show()
 # ----------------------------------------------------------------
 
 
