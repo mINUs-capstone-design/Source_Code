05eca107e4ce1a8e6e0f2cafa6cfddddc561e3bc
mINUs <jx090425@gmail.com>
2024-05-15 07:09:09 +0900

    test

---
+++ tts_test.py
@@ -0,0 +1,43 @@
+# ----------------------------------------------------------------
+from google.cloud import texttospeech
+from google.oauth2 import service_account
+# ----------------------------------------------------------------
+
+# ----------------------------------------------------------------
+# TTS 변환 기능 함수
+def run_tts():
+
+    credentials = service_account.Credentials.from_service_account_file('majestic-cairn-422006-c4-adf3cfa75c37.json')
+
+    client = texttospeech.TextToSpeechClient(credentials=credentials)
+    
+    #text_block = input("텍스트 입력 : ")
+    
+    synthesis_input = texttospeech.SynthesisInput(text="사과")
+
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="ko-KR",
+        name="ko-KR-Standard-A",
+        ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+    
+    #OUTPUT = input("저장할 파일이름 & 확장자 입력 : ")
+    
+    with open("TTS_woman_record.wav", "wb") as out:
+        out.write(response.audio_content)
+        print(f'Audio content written to file "TTS_record.wav"')
+# ----------------------------------------------------------------
+
+# ----------------------------------------------------------------
+# main문에서 run_tts 함수 실행
+if __name__ == "__main__":
+    run_tts()
+# ----------------------------------------------------------------
