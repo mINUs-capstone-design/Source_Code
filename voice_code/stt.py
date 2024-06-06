9f0dc0f3e52fad130ef0fc832bc75896f97dc53c
DESKTOP-FDBRA0R\mscho <mscho>
2024-06-06 12:09:04 +0900

    숫자 매핑

--- voice_code/stt.py
+++ voice_code/stt.py
@@ -3,6 +3,30 @@ from google.cloud import speech_v1p1beta1 as speech
 from google.oauth2 import service_account
 from pydub import AudioSegment
 
+pronunciation_map = {
+    "영": "영",
+    "일": "일",
+    "이": "이",
+    "삼": "삼",
+    "사": "사",
+    "오": "오",
+    "육": "육",
+    "칠": "칠",
+    "팔": "팔",
+    "구": "구",
+    "십": "십",
+    "공": "공",
+    "하나": "하나",
+    "둘": "둘",
+    "셋": "셋",
+    "넷": "넷",
+    "다섯": "다섯",
+    "여섯": "여섯",
+    "일곱": "일곱",
+    "여덟": "여덟",
+    "아홉": "아홉",
+    "열": "열",
+}
 #--------------------------------------------------
 def transcribe_audio(audio_file_path):
     credentials = service_account.Credentials.from_service_account_file('speech-to-text-425408-a797567b8e72.json')
@@ -29,8 +53,10 @@ def transcribe_audio(audio_file_path):
     response = client.recognize(config=config, audio=recognition_audio)
 
     for result in response.results:
-        print("Transcript: {}".format(result.alternatives[0].transcript))
-        return format(result.alternatives[0].transcript)
+        transcript = result.alternatives[0].transcript.strip()
+        print("Transcript: {}",transcript)
+        transcript = "".join([pronunciation_map.get(word, word) for word in transcript.split()])
+        return transcript
 
 # 예제로 사용할 오디오 파일의 경로
 # audio_file_path = "record.wav"
