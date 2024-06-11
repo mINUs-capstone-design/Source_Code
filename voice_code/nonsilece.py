51c51aabadf73e8ea6dd900f482fe26041637f4d
DESKTOP-FDBRA0R\mscho <mscho>
2024-06-11 12:07:22 +0900

    소리가 나는 부분 체크

---
+++ voice_code/nonsilece.py
@@ -0,0 +1,27 @@
+from pydub import AudioSegment
+from pydub.silence import detect_nonsilent
+
+
+def nonsilent_parts(input_file):
+    # 오디오 파일 로드
+    audio = AudioSegment.from_file(input_file)
+    min_silence_len = 1000
+    silence_thresh = -100
+    # 소리가 나는 구간 탐지
+    nonsilent_ranges = detect_nonsilent(audio, min_silence_len=min_silence_len, silence_thresh=silence_thresh)
+
+    # 소리가 나는 구간 추출
+    nonsilent_audio = AudioSegment.empty()
+    for start, end in nonsilent_ranges:
+        nonsilent_audio += audio[start:end]
+
+    # 결과 저장
+    nonsilent_audio.export(input_file, format="wav")
+    print(f"파일이 성공적으로 저장되었습니다: {input_file}")
+
+
+# 사용 예시
+# input_file = "path/to/your/input_file.wav"
+# output_file = "path/to/your/output_file.wav"
+# extract_nonsilent_parts(input_file, output_file)
+ss
